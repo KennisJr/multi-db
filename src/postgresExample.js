6ee45d3b9952c17fa08d599e4ec593c5e9c0162d
edson_kennis <edson.k.santos@outlook.com>
2023-03-08 19:37:38 -0300

    -i sequalize with postgres and create postgresExam

---
+++ src/postgresExample.js
@@ -0,0 +1,43 @@
+// npm install sequelize 
+// npm install pg-hstore
+const Sequelize = require('sequelize')
+const driver = new Sequelize(
+    'livros',
+    'edsonkennis',
+    '12345',
+    {
+        host: 'localhost',
+        dialect: 'postgres',
+        quoteIdentifiers: false,
+        operatorAliases: false
+    }
+)
+
+async function main() {
+    const Livros = driver.define('livros', {
+        id: {
+            type: Sequelize.INTEGER,
+            required: true,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: {
+            type: Sequelize.STRING,
+            required: true
+        },
+        autor: {
+            type: Sequelize.STRING,
+            required: true
+        }
+        }, {
+            tableName: 'TB_LIVROS',
+            freezeTableName: false,
+            timestamps: false
+        })
+    await Livros.sync();
+
+    const result = await Livros.findAll({ raw: true });
+    console.log(result);
+}
+
+main();
